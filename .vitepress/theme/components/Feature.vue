3513768bca5b399e27188bc6adf7b2d4eee2325d
Mahoo12138 <mahoo12138@qq.com>
2025-04-18 13:08:27 +0800

    refactor(layout): improve grid system and adjust feature component styles
    - Update grid system to use flexible gutters
    - Adjust Feature component to use new grid system
    - Refactor box styles to include gutter classes
    - Update style.scss to import grid styles before box styles

--- .vitepress/theme/components/Feature.vue
+++ .vitepress/theme/components/Feature.vue
@@ -8,51 +8,51 @@
             <div class="row g-4 pt-5 row-cols-1 row-cols-md-2 row-cols-lg-3">
                 <div class="col">
                     <div
-                        class="feature-icon d-inline-flex align-items-center justify-content-center text-bg-primary bg-gradient fs-2 mb-3">
+                        class="feature-icon d-inline-flex align-items-center justify-content-center text-bg-primary bg-gradient feature-title mb-3">
                         <i class="bi bi-coin"></i>
                     </div>
-                    <h3 class="fs-2 text-body-emphasis">无氪金体验</h3>
+                    <h3 class="feature-title text-body-emphasis">无氪金体验</h3>
                     <p>纯净式公益运营，没有任何充值入口。我们尽最大的努力确保游戏公平，只为更好的游戏体验。</p>
                 </div>
                 <div class="col">
                     <div
-                        class="feature-icon d-inline-flex align-items-center justify-content-center text-bg-primary bg-gradient fs-2 mb-3">
+                        class="feature-icon d-inline-flex align-items-center justify-content-center text-bg-primary bg-gradient feature-title mb-3">
                         <i class="bi bi-tree"></i>
                     </div>
-                    <h3 class="fs-2 text-body-emphasis">主打纯净式生存</h3>
+                    <h3 class="feature-title text-body-emphasis">主打纯净式生存</h3>
                     <p>道生一，一生二，三生万物。基于 Java 国际版的原版体验，让 Minecraft 回归生存玩法。</p>
 
                 </div>
                 <div class="col">
                     <div
-                        class="feature-icon d-inline-flex align-items-center justify-content-center text-bg-primary bg-gradient fs-2 mb-3">
+                        class="feature-icon d-inline-flex align-items-center justify-content-center text-bg-primary bg-gradient feature-title mb-3">
                         <i class="bi bi-people-fill"></i>
                     </div>
-                    <h3 class="fs-2 text-body-emphasis">开放共享式社区</h3>
+                    <h3 class="feature-title text-body-emphasis">开放共享式社区</h3>
                     <p>Mahoo Town 的玩家们来自世界各地，他们团结友爱、互助进取。这里，是激情四射、想法碰撞的天堂。</p>
                 </div>
                 <div class="col">
                     <div
-                        class="feature-icon d-inline-flex align-items-center justify-content-center text-bg-primary bg-gradient fs-2 mb-3">
+                        class="feature-icon d-inline-flex align-items-center justify-content-center text-bg-primary bg-gradient feature-title mb-3">
                         <i class="bi bi-houses-fill"></i>
                     </div>
-                    <h3 class="fs-2 text-body-emphasis">田园牧歌式建筑</h3>
+                    <h3 class="feature-title text-body-emphasis">田园牧歌式建筑</h3>
                     <p>每一处建筑都经过了精心设计，美观而实用。这里是传统与现代的结合，是无限智慧的结晶。</p>
                 </div>
                 <div class="col">
                     <div
-                        class="feature-icon d-inline-flex align-items-center justify-content-center text-bg-primary bg-gradient fs-2 mb-3">
+                        class="feature-icon d-inline-flex align-items-center justify-content-center text-bg-primary bg-gradient feature-title mb-3">
                         <i class="bi bi-cpu-fill"></i>
                     </div>
-                    <h3 class="fs-2 text-body-emphasis">弱鸡服务器配置</h3>
+                    <h3 class="feature-title text-body-emphasis">弱鸡服务器配置</h3>
                     <p>服务器位于中国上海，为一台搭载 AMD Ryzen5 4600U 的旧笔记本，并有 12M 的带宽，多人同服应该会卡顿。</p>
                 </div>
                 <div class="col">
                     <div
-                        class="feature-icon d-inline-flex align-items-center justify-content-center text-bg-primary bg-gradient fs-2 mb-3">
+                        class="feature-icon d-inline-flex align-items-center justify-content-center text-bg-primary bg-gradient feature-title mb-3">
                         <i class="bi bi-building"></i>
                     </div>
-                    <h3 class="fs-2 text-body-emphasis">躺平运营</h3>
+                    <h3 class="feature-title text-body-emphasis">躺平运营</h3>
                     <p>Mahoo Town 由 Mahoo12138 个人运营，“腐竹”应该不跑路。</p>
                 </div>
             </div>
@@ -63,5 +63,10 @@
 <style>
 .feature {
     background-color: aliceblue;
+
+    .feature-title {
+        font-size: 2rem;
+        margin-bottom: .5rem;
+    }
 }
 </style>
